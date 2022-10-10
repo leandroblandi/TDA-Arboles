ce0866db01a9b3070995ae5ce71ebfb945dc6d30
Leandro Blandi <59542421+leandroblandi@users.noreply.github.com>
2022-10-10 10:52:34 -0300

    Cambios en las entradas
    Se sustituyó la función gets por fgets.
    Sin embargo, debido a que la función fgets nos genera un \n al final del string, nos ayudamos con la función strtok, que busca un delimitador y recorta el string hasta ése mismo delimitador.
    
    Revisar línea 46.
    
    Artículo que habla más sobre el tema.
    https://stackoverflow.com/questions/2693776/removing-trailing-newline-character-from-fgets-input

--- arbol.c
+++ arbol.c
@@ -43,7 +43,7 @@ Arbol crearArbolPorTeclado()
 
     printf("\nIngrese nombre del arbol: ");
     fflush(stdin);
-    gets(nombre);
+    strtok(fgets(nombre, 25, stdin));
 
     printf("\nIngrese la edad de \"%s\": ", nombre);
     scanf("%d", &edad);
